27598b30d7529056ef147abd43772c666bf4a3df
Reales Myles <rclarence123@gmail.com>
2024-03-26 23:39:20 -0500

    Initial Commit

---
+++ Tablas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tablas
+//
+//  Created by Reales Rectoro Myles Clarence on 26/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
